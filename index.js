44f2ff221290b1f51f23cd240b91ad20a3b56bdc
Alausa Aishat <qtaisha.ao@gmail.com>
2022-06-13 10:07:12 +0100

    arrow functions lab

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function(num1, num2) {
+   num1 / num2;
+    return 2000/100;  
+}
+
+
+const square = (x) => x**2;
+square(x);
+
+function add(a, b) {
+    `adding ${a}`;
+    `adding ${b}`;
+    return a + b;
+}
+add(3,4);
